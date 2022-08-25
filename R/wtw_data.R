82528a7f25159d75494a0a4d5f58e3c0667fbf58
Steve Markham <Nattereri@users.noreply.github.com>
2022-08-25 15:10:04 +0100

    wtw_data added

---
+++ R/wtw_data.R
@@ -0,0 +1,11 @@
+#' A data set of water treatment works and reported determinands
+#'
+#' A data set of nine water treatment works with the following determinands:
+#' Coliforms, Colony Count 21, Colony Count 37, Colour, Conductivity, E.coli,
+#' Free Chlorine, Odour, pH, Taste, Temperature, Total Chlorine and Turbidity
+#'
+#' @docType data
+#'
+#' @usage data(wtw_data)
+#'
+"wtw_data"
